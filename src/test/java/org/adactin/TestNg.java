199757a1c1a1e8917e423a8ffa2667296b51fc13
surendran1828 <surendransurea18@gmail.com>
2021-01-26 10:32:11 +0530

    new

--- src/test/java/org/adactin/TestNg.java
+++ src/test/java/org/adactin/TestNg.java
@@ -49,5 +49,10 @@ public class TestNg extends Base {
 		WebElement j = driver.findElement(By.xpath("//input[@name='order_no']"));
 	    System.out.println("your booking id is "+j.getAttribute("value")); 
 	}
+	@Test
+	private void test5() {
+		System.out.println("done");
+		System.out.println("done");
+	}
 	
 }
